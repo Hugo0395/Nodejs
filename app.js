e0d5f00df2b16eeab67c9d24d074f24d154a0662
Hugo Tapia <hugotfdev0395@gmail.com>
2023-09-07 19:57:27 -0400

    Fixes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 //importar base de datos
-import db from './database/db.js';
+
 //importar enrutador
 import blogRoutes from './routes/routes.js';
 import { json } from 'sequelize';
@@ -12,14 +12,6 @@ app.use(express.json());
 app.use('/blogs',blogRoutes);
 
 //definir conexion
-
-try {
-    await db.authenticate();
-    console.log("Conexion exitosa a la base de datos");
-} catch (error) {
-    console.log("Conexion erronea a la base de datos");
-}
-
 
 app.get('/', (req, res)=>{
     res.send("hola mundo esto es una prueba")
